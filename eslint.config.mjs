e18cf89825fe736288b0af8b02dee9d599ba2791
brabli <67018167+brabli@users.noreply.github.com>
2025-05-04 12:37:12 +0100

    Work on config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,19 @@
-import js from "@eslint/js";
-import { defineConfig } from "eslint/config";
-import globals from "globals";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-export default defineConfig([
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    tseslint.configs.strict,
     {
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-            }
-        },
-        files: ["**/*.{js,mjs,cjs,ts}"],
-        plugins: { js },
-        extends: ["js/recommended"],
         rules: {
             "prefer-const": "warn",
-            "indent": ["warn", 4],
-
+            "indent": [
+                "warn", 4
+            ],
             "brace-style": [
                 "error"
             ],
-
             "linebreak-style": [
                 "error",
                 "unix"
@@ -30,16 +24,12 @@ export default defineConfig([
                 "always"
             ],
 
-            "comma-spacing": "error",
-            "curly": "error",
+            "comma-spacing": "warn",
+            "curly": "warn",
             "eqeqeq": "error",
             "eol-last": ["warn", "always"],
 
-            "no-console": ["warn", { "allow": ["error"] }],
-
             "keyword-spacing": ["warn"],
         },
-
-    },
-]);
-
+    }
+);

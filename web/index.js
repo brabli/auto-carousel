5f2b8a2a12bb5c240b769b0e935374356107c28e
brabli <67018167+brabli@users.noreply.github.com>
2025-05-10 16:20:54 +0100

    Add test webpage

---
+++ web/index.js
@@ -0,0 +1,12 @@
+import { AutoCarousel } from "../dist/index.mjs";
+
+const wrapper = document.querySelector('.wrapper');
+if (null === wrapper) {
+    throw new Error("No .wrapper class was found.");
+}
+
+new AutoCarousel(wrapper, {
+    speed: 1,
+    gap: 32,
+    direction: 'left'
+});

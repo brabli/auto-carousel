a0ee0146ee3d7d57d439cf9b2a829a2e1724414b
brabli <67018167+brabli@users.noreply.github.com>
2025-05-10 23:09:53 +0100

    Tweak options

--- web/index.js
+++ web/index.js
@@ -3,9 +3,10 @@ import { AutoCarousel } from "../dist/index.mjs";
 const textBoxes = document.querySelector(".text-boxes");
 
 new AutoCarousel(textBoxes, {
-    speed: 1,
+    speed: 2,
     gap: 32,
     direction: "left",
+    debug: true,
 });
 
 const images = document.querySelector(".images");

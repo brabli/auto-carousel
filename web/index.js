9fd04ca1e39209b78afa5947cb277f75a3395a8a
brabli <67018167+brabli@users.noreply.github.com>
2025-05-11 21:03:46 +0100

    Simplify test webpage

--- web/index.js
+++ web/index.js
@@ -7,23 +7,22 @@ new AutoCarousel(textBoxes, {
     gap: 32,
     direction: "right",
     debug: true,
-    containerSelector: ".container",
 });
-
-const images = document.querySelector(".images");
-
-new AutoCarousel(images, {
-    speed: 1,
-    gap: 32,
-    direction: "right",
-    containerSelector: ".container",
-});
-
-const small = document.querySelector(".small");
-
-new AutoCarousel(small, {
-    speed: 1,
-    gap: 8,
-    direction: "right",
-    containerSelector: ".container",
-});
+//
+// const images = document.querySelector(".images");
+//
+// new AutoCarousel(images, {
+//     speed: 1,
+//     gap: 32,
+//     direction: "right",
+//     containerSelector: ".container",
+// });
+//
+// const small = document.querySelector(".small");
+//
+// new AutoCarousel(small, {
+//     speed: 1,
+//     gap: 8,
+//     direction: "right",
+//     containerSelector: ".container",
+// });
